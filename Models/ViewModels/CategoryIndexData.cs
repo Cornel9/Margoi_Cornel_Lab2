3a9bea304bf795476bad0153f1faf6730d7c171d
Cornel M <cornel_9@yahoo.com>
2024-11-17 11:30:43 +0200

    Good!

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Margoi_Cornel_Lab2.Models;
+
+namespace Margoi_Cornel_Lab2.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+    }
+}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Margoi_Cornel_Lab2.Models;
+
+namespace Margoi_Cornel_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
